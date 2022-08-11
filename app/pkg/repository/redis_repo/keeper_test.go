9acc2b2e64453ee2e00ccdace10188ecb3277724
asssswv <tozhimukhammedov.aa@phystech.edu>
2022-08-12 02:32:54 +0300

    add some test

--- app/pkg/repository/redis_repo/keeper_test.go
+++ app/pkg/repository/redis_repo/keeper_test.go
@@ -8,6 +8,51 @@ import (
 	"testing"
 )
 
+func TestRedisKeeper_Get(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+	defer func(rdb *redis.Client) {
+		err := rdb.Close()
+		if err != nil {
+			logrus.Fatal("failed to close rdb")
+		}
+	}(rdb)
+
+	r := NewRedisKeeper(rdb)
+
+	type mockBehavior func(key string)
+
+	testTable := []struct {
+		name         string
+		mockBehavior mockBehavior
+		key          string
+		wantErr      bool
+	}{
+		{
+			name: "FAIL",
+			key:  "test",
+			mockBehavior: func(key string) {
+				mock.ExpectGet(key).RedisNil()
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehavior(testCase.key)
+
+			message, err := r.Get(testCase.key)
+			if testCase.wantErr {
+				assert.Equal(t, message, "")
+				assert.NotEqual(t, err, redis.Nil)
+			} else {
+				assert.NotEqual(t, message, "")
+				assert.Equal(t, err, redis.Nil)
+			}
+		})
+	}
+}
+
 func TestRedisKeeper_Set(t *testing.T) {
 	rdb, mock := redismock.NewClientMock()
 	defer func(rdb *redis.Client) {
@@ -50,7 +95,7 @@ func TestRedisKeeper_Set(t *testing.T) {
 
 			err := r.Set(testCase.args.key, testCase.args.message)
 			if testCase.wantErr {
-				assert.Error(t, err)
+				assert.NotEqual(t, err, redis.Nil)
 			} else {
 				assert.Equal(t, err, redis.Nil)
 			}
