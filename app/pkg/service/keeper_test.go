11d4e738d2b40cdd0877bb87f082e579dce36abd
asssswv <tozhimukhammedov.aa@phystech.edu>
2022-08-25 08:17:51 +0300

    add testing ttl

--- app/pkg/service/keeper_test.go
+++ app/pkg/service/keeper_test.go
@@ -1,9 +1,11 @@
 package service
 
 import (
-	"github.com/stretchr/testify/assert"
 	"secrets_keeper/app/pkg/repository/redis_repo"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // RaceTest
@@ -43,4 +45,56 @@ func TestKeeperService_Get(t *testing.T) {
 
 	assert.Equal(t, nil, firstErr)
 	assert.Equal(t, "message not found", secondErr.Error())
+}
+
+// ttl test
+func TestKeeperService_Set(t *testing.T) {
+	rdb := redis_repo.NewRedisClient(redis_repo.Config{
+		Host:     "localhost",
+		Port:     "6379",
+		Password: "pass",
+	})
+
+	repos := redis_repo.NewRepository(rdb)
+	services := NewService(repos)
+
+	testTable := []struct {
+		name    string
+		message string
+		key     string
+		ttl     int
+		wantErr bool
+	}{
+		{
+			name:    "FAIL",
+			message: "first",
+			key:     "first_1",
+			ttl:     2,
+			wantErr: true,
+		},
+		{
+			name:    "OK",
+			message: "second",
+			key:     "second_2",
+			ttl:     100,
+			wantErr: false,
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			services.Keeper.Set(testCase.key, testCase.message, testCase.ttl)
+			time.Sleep(3 * time.Second)
+
+			message, err := services.Keeper.Get(testCase.key)
+			if testCase.wantErr {
+				assert.NotEqual(t, err, nil)
+				assert.Equal(t, message, "")
+			} else {
+				// fmt.Println("===================", err)
+				assert.Equal(t, err, nil)
+				assert.NotEqual(t, message, nil)
+			}
+		})
+	}
 }
